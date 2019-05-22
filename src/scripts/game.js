f1eec361901153103d037dfc58813b790661b54a
Emmanuel Orozco <emmanuelorozco@TRFRLTA167.local>
2019-05-22 18:37:29 +0200

    repo init

---
+++ src/scripts/game.js
@@ -0,0 +1,31 @@
+import 'phaser'
+import '@babel/polyfill'
+
+import MainScene from './scenes/mainScene'
+import PreloadScene from './scenes/preloadScene'
+
+const DEFAULT_WIDTH = 800
+const DEFAULT_HEIGHT = 600
+
+const config = {
+  backgroundColor: '#ffffff',
+  scale: {
+    parent: 'phaser-game',
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: DEFAULT_WIDTH,
+    height: DEFAULT_HEIGHT
+  },
+  scene: [PreloadScene],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: { y: 400 }
+    }
+  }
+}
+
+window.addEventListener('load', () => {
+  let game = new Phaser.Game(config)
+})
