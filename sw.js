ccc1a360ebaee416e0b65ee3d855330af6178a92
Yann Vaillant <yann@vaillant.im>
2019-05-23 12:51:55 +0200

    Add first release

---
+++ sw.js
@@ -0,0 +1,11 @@
+importScripts("precache-manifest.5a94d70b2287dd0230ccb6ff03908dfb.js", "https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
