e4f477b5343545aac875fd9e968d9c0021a895b4
Yann Vaillant <yann@vaillant.im>
2019-05-23 12:51:55 +0200

    Add first release

---
+++ dist/precache-manifest.5a94d70b2287dd0230ccb6ff03908dfb.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49115b6bc4e3ee55a79eed76f962b7f6",
+    "url": "assets/img/logo.png"
+  },
+  {
+    "revision": "4b62b6ac07cd40c0293a",
+    "url": "main.aef78009000b60e39131.bundle.js"
+  },
+  {
+    "revision": "75a44a3527d670c61b07",
+    "url": "vendors.8b5e4c589ae4ecd25999.bundle.js"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "sw.js"
+  },
+  {
+    "revision": "bf38d0c9760162342f12cd9b44fbf4ef",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b887c333d5ec8274b78d0851e7b3337b",
+    "url": "assets/img/phaser-logo.png"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "8bdcc486cda9b423f50e886f2ddb6604",
+    "url": "icons/icons-512.png"
+  },
+  {
+    "revision": "2ffbc23293ee8a797bc61e9c02534206",
+    "url": "icons/icons-192.png"
+  },
+  {
+    "revision": "32a5a114380468dac4e369456e8defa8",
+    "url": "index.html"
+  },
+  {
+    "revision": "6617dd6c0fa91b5ee462203715beb8fe",
+    "url": "assets/img/trees/round.png"
+  },
+  {
+    "revision": "dc7ded0f3ead7ee49da972b9b345cde0",
+    "url": "assets/img/trees/spreading.png"
+  },
+  {
+    "revision": "67dca527dd0a17ebf574008d58f311fa",
+    "url": "assets/img/trees/vase.png"
+  },
+  {
+    "revision": "4becfd2b272aba5d28cb526a7defb4f8",
+    "url": "assets/img/trees/open.png"
+  },
+  {
+    "revision": "cb8fe21064e648d4fd4aa71e2a5d339d",
+    "url": "assets/img/sky.png"
+  },
+  {
+    "revision": "85e9f9ef1d986c5e345b882bedfcde08",
+    "url": "assets/img/trees/pyramid.png"
+  },
+  {
+    "revision": "e66059e310c1b371ce5d49be47cee42d",
+    "url": "assets/img/trees/oval.png"
+  },
+  {
+    "revision": "5b0bdabd0d14c46e294879695056a427",
+    "url": "assets/img/trees/columnnar.png"
+  },
+  {
+    "revision": "3bbfad7412f062fcbae5984254093294",
+    "url": "assets/img/star.png"
+  },
+  {
+    "revision": "cd75750e78fce666753c476c4e1d58dc",
+    "url": "assets/img/trees/branched.png"
+  },
+  {
+    "revision": "f09343864693380de2d4e100c1cc53ec",
+    "url": "assets/img/trees/cone.png"
+  },
+  {
+    "revision": "fc7d654a5cbca97fbe6091f9b37ac135",
+    "url": "assets/img/platform.png"
+  },
+  {
+    "revision": "8615288f2693d67595407abdd366fa61",
+    "url": "assets/img/dude.png"
+  },
+  {
+    "revision": "185c1ec968402aeafffa49fc95956269",
+    "url": "assets/img/bomb.png"
+  },
+  {
+    "revision": "4384735dac59cf06977630a4f60a4fe6",
+    "url": "assets/img/.DS_Store"
+  },
+  {
+    "revision": "bcab8973d40240fc7612e1b8f1c9b9f9",
+    "url": "assets/.DS_Store"
+  }
+]);
